283fa0133f2750903ae429a954f9f42c4abbbe6f
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-20 21:39:39 -0400

    ThirdCommit

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`)
 app.use('/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`));
 app.use(express.static(`${__dirname}/public`));
 
+app.engine(
+    "handlebars",
+    hbs.engine({
+        layoutsDir: `${__dirname}/views`,
+        partialsDir: `${__dirname}/views/partials`
+    })
+);
+
+
+app.listen(3000, () => console.log("Servidor activo http://localhost:3000"));
+
+app.get("/", (req, res) => {
+    res.render("main", {
+        productos: [
+            "banana",
+            "cebollas",
+            "pimenton",
+            "papas",
+            "lechuga",
+            "tomate"
+        ]
+    });
+});
